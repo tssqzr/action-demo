e46f220125508c7db80522ecdcbc80ae13ac109a
tssqzr <1214661466@qq.com>
2020-03-18 11:32:49 +0800

    add

---
+++ js/creative.js
@@ -0,0 +1,93 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
+
+  // Magnific popup calls
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+  // countTime();
+})(jQuery); // End of use strict
+
+function countTime() {
+  let date = new Date();
+  let now = date.getTime();
+
+  let endDate = new Date("2018/10/09 10:00:00");
+  let end = endDate.getTime();
+
+  let leftTime = end - now;
+
+  let d = 0, h = 0, s = 0;
+  if (leftTime >= 0) {
+    d = Math.floor(leftTime / 1000 / 60 / 60 / 24);
+    h = Math.floor(leftTime / 1000 / 60 / 60 % 24);
+  } else {
+    clearTimeout(countTime);
+    return false;
+  }
+  document.getElementById("_d").innerHTML = d;
+  document.getElementById("_h").innerHTML = h;
+  document.getElementById("_ds").innerHTML = d;
+  document.getElementById("_hs").innerHTML = h;
+  setTimeout(countTime, 1000);
+}
